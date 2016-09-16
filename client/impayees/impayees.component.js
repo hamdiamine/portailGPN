ef09ee0937e6e8aee10694f2bbb50130db04e6ca
hkh <hamdi.amine.hkh@gmail.com>
2016-09-16 02:12:24 +0100

    - Terminer impayées - Terminer notif - Corriger fiche facture - Corriger fiche releve - Corriger fiche compte client

--- client/impayees/impayees.component.js
+++ client/impayees/impayees.component.js
@@ -3,12 +3,27 @@ angular.module('portailGP').directive('impayees', function () {
     restrict: 'E',
     templateUrl: 'client/impayees/impayees.html',
     controllerAs: 'impayeesctrl',
-    controller: function ($scope, $stateParams, $reactive) {
+    controller: function ($scope, $stateParams, $reactive, $state) {
       $reactive(this).attach($scope);
 
-      this.helpers({
+      var idClient = Session.get('client').id;
+      if(!idClient||idClient===null){
+        $state.go('accueil');
+      }
 
+      this.subscribe('factures');
+
+      this.helpers({
+        impayees:()=>{
+          return Factures.find({idPayeur:idClient,restantDu:{$gt:0}});
+        }
       });
+
+      this.ficheFacture=($index)=>{
+        var idFacture = this.impayees[$index].idFacture;
+        Session.set('idFacture', idFacture);
+        $state.go('fichefacture');
+      }
 
     }
   }
